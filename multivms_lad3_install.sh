4d1ae06d63e22e7c2815171502cdedc213b90951
Marin Nedea <marinnedea@users.noreply.github.com>
2018-06-13 12:53:33 +0300

    Update multivms_lad3_install.sh

--- multivms_lad3_install.sh
+++ multivms_lad3_install.sh
@@ -1,45 +1,6 @@
 #!/usr/bin/env
 
-#########################################################
-#	This script will use the details you input	#
-#	to install LAD 3.0 on all Linux VMs in a 	#
-#	specific subscription ID. 			#
-# Requirements:						#
-#	- PrivateConfig.json and publicConfig files 	#
-#	must be located in the same directory with	#
-#	this script.					#
-# 	- the details for LAD configuration must be	#
-#	set in advance in the Azure Portal.		#
-# Disclaimer: 						#
-# 	- This script is provided "as it is" without	#
-#	any waranty. Please make sure you teste it	#
-#	in a non production environment before using	#
-#	in a production one.				#
-#	- I or the company I work for won't take	#
-#	any responsability for the damage the use of	#
-# 	this script may cause.				#
-#							#
-#		USE IT AT YOUR OWN RISK!		#
-#							#
-#########################################################
-# Script execution: 
-# 	- Save the script in a location of you choice, e.g:
-#	/home/you_username
-#	- make the script executable: chmod +x script_name.sh
-#	- execute the script: ./script_name.sh
-# Note: - replace script_name.sh with the actual script name.
-# 	- all VMs will be updated one by one, the script 
-#	doesn't run any parallel tasks, therefore, for a
-#	large pool of VMs, expect to take a long time to
-#	complete. Don't kill the script execution unless
-#	the script runs into errors.
-# 
-# Documentation:   
-#
-#	https://docs.microsoft.com/en-us/azure/virtual-machines/extensions/diagnostics-linux#installing-and-configuring-lad-30-via-cli
-#	https://docs.microsoft.com/en-us/cli/azure/vm/extension?view=azure-cli-latest#az-vm-extension-list
-#
-#########################################################
+# Check the README.md file for instructions
 
 # Setting the logfile location
 logfile=/var/log/lad_install_output.log
